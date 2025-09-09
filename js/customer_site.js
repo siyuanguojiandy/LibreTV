16c8f67aad7491b28778ee37c66e269d0c4df343
workers yyds <133344246+siyuanguojiandy@users.noreply.github.com>
2025-09-09 15:13:36 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -36,9 +36,9 @@ const CUSTOMER_SITES = {
       "api": "https://api.zuidapi.com/api.php/provide/vod",
       "name": "最大资源"
     },
-    wujin: {
-      "api": "https://api.wujinapi.me/api.php/provide/vod",
-      "name": "无尽资源"
+    wwdj: {
+      "api": "https://api.wwzy.tv/api.php/provide/vod",
+      "name": "旺旺短剧",
     },
 };
 
